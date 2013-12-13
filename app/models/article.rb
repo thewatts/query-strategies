67347bbd205c6c3ab4b7fd1ecd62d726dced5cdc
Nathaniel Watts <reg@nathanielwatts.com>
2013-12-12 19:18:45 -0700

    add default scope

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ActiveRecord::Base
   has_many :taggings
   has_many :tags, :through => :taggings
 
-  #default_scope :include => [:comments, :tags]
+  default_scope :include => { :comments => :approval }
 
   def to_s
     return title
